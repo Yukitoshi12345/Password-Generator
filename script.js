6b6bb154eeca42fef7d4c1ae227e5c7e94017d80
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2023-12-02 01:34:01 +1100

    asked a question to pick number of characters from 8 to 128

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+var passwordCharacterCount;
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharacter = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +17,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  passwordCharacterCount = prompt("Please select the desired password length, ranging from 8 to 128 characters.")
+  console.log("Password length: " + passwordCharacterCount);
+}
