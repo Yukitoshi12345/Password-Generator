e121a0e3eef7ea1f4659c43949bb1ed7d9b78731
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2023-12-03 14:22:03 +1100

    added upper, lower, special characters, and numbers in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,29 @@ var generateBtn = document.querySelector("#generate");
 
 var passwordCharacterCount;
 
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// There are 2 ways of producing upperCase. One is simply:
+// var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V","W", "X", "Y","Z"];
+// The other way is by using functions: 
+function toUpperCase(lowerCase) {
+  var empty = [];
+  for (var char of lowerCase) {
+    empty.push(char.toUpperCase());
+  }
+  return empty;
+}
+
+var upperCase = toUpperCase(lowerCase);
+// To test upperCase runs properly:
+console.log(upperCase);
+
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharacter = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];;
 
+// To test the specialCharacter runs properly:
 console.log(specialCharacter)
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
